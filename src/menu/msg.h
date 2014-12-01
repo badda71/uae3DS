0c46fffe8f89b0805a023da797b1549cff7c53ad
Zear <zeartul@gmail.com>
2014-12-01 23:39:07 +0100

    Changed the scrolling credits text to reflect the authors of the GCW Zero port and clarify on the code base origin

--- src/menu/msg.h
+++ src/menu/msg.h
@@ -6,6 +6,10 @@ static const char *menu_msg="                         Amiga emulator "
  "for Dingoo "
 #endif
 #endif
+#ifdef GCW0
+ "by Chui - based on UAE 0.8.22. GCW Zero port by Nebuleon and Zear. UAE4ALL logo and minor gfx changes by Hi-Ban. Release 1.";
+#else
  "by Chui.                    Third release Candidate.                               GPL License.                                          ";
+#endif
 
 #define MAX_SCROLL_MSG (-1500)
